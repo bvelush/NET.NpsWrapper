5cdcc755b5ce96ebc72758e18e9591054fdad61d
Dan Velush <bvelush@gmail.com>
2025-09-01 23:31:55 +0300

    Logging interface improved

--- NpsWrapperNET/NpsWrapperNET.cs
+++ NpsWrapperNET/NpsWrapperNET.cs
@@ -14,15 +14,14 @@ using System.Linq;
 using SDOIASLib;
 using System.IO;
 using System.Xml.XPath;
+using System.Data.Common;
 //using Auth_WatchGuard;
 
-namespace NpsWrapperNET
-{
+namespace NpsWrapperNET {
     /// <summary>
     /// Provides the entry points for the NPS service (indirectly called by the C++/CLR wrapper).
     /// </summary>
-    public class NpsWrapper
-    {
+    public class NpsWrapper {
         /// <value>EventLog APP source.</value>
         const string APP_NAME = "NPS-Wrapper.NET";
         /// <value>Radius CallbackId value triggering MFA request. Must match attribute set by NPS upon Access-Request.</value>
@@ -35,16 +34,14 @@ namespace NpsWrapperNET
         /// <remarks>Use RadiusExtensionInit to perform any initialization operations for the Extension DLL</remarks>
         /// </summary>
         /// <returns>A return value other then 0 will cause NPS to fail to start</returns>
-        public static uint RadiusExtensionInit()
-        {
-            WriteEventLog(APP_NAME, ".NET RadiusExtensionInit called", new List<string>(), EventLogEntryType.Information);
-            if (initCount == 0)
-            {
+        public static uint RadiusExtensionInit() {
+            WriteEventLog(EventLogEntryType.Information, "RadiusExtensionInit called");
+            if (initCount == 0) {
                 initCount++;
                 if (GetRADIUSClientsXMLconf().Count > 0)
-                    WriteEventLog(APP_NAME, "Initialized", new List<string>(), EventLogEntryType.Information);
+                    WriteEventLog(EventLogEntryType.Information, "Initialized");
                 else
-                    WriteEventLog(APP_NAME, "Initialized", new List<string> { $"No RADIUS Clients found" }, EventLogEntryType.Warning);
+                    WriteEventLog(EventLogEntryType.Warning, "Initialized but 'GetRADIUSClientsXMLconf' count is 0");
             }
             return 0;
         }
@@ -53,75 +50,70 @@ namespace NpsWrapperNET
         /// <para>Called by NPS prior to unloading the Extension DLL</para>
         /// <remarks>Use RadiusExtensionTerm to perform any clean-up operations for the Extension DLL</remarks>
         /// </summary>
-        public static void RadiusExtensionTerm()
-        {
+        public static void RadiusExtensionTerm() {
             initCount--;
-            if (initCount == 0)
-                WriteEventLog(APP_NAME, "Terminated", new List<string>(), EventLogEntryType.Information);
+            if (initCount == 0) {
+                WriteEventLog(EventLogEntryType.Information, "RadiusExtensionInit called");
+            }
         }
         /// <summary>
         /// Called by the NPS host to process an authentication or authorization request.
         /// </summary>
         /// <param name="ecbPointer">Pointer to the extension control block.</param>
         /// <returns>0 if all plugins were processed successfully or 5 (access denied) when at least one of the plugins failed.</returns>
-        public static uint RadiusExtensionProcess2(IntPtr ecbPointer)
-        {
-            //WriteEventLog(APP_NAME, "RadiusExtensionProcess2 called", new List<string>(), EventLogEntryType.Warning);
+        public static uint RadiusExtensionProcess2(IntPtr ecbPointer) {
             var control = new ExtensionControl(ecbPointer);
             string userName = string.Empty;
             List<string> logMessage = new List<string>
             {
-                "+NPS request start",
+                "NPS request start",
                 "-ExtensionPoint: " + control.ExtensionPoint.ToString(),
                 "-RequestType: " + control.RequestType.ToString(),
                 "-ResponseType: " + control.ResponseType.ToString()
             };
-            WriteEventLog(APP_NAME, "RadiusExtensionProcess2 called with params:", new List<string>() { "aaa" }, EventLogEntryType.Warning);
-            /* Authentication request */
-            if ((control.ExtensionPoint == RadiusExtensionPoint.Authentication) && (control.RequestType == RadiusCode.AccessRequest))
-            {
-                if (control.ResponseType == RadiusCode.Unknown)
-                {
-                    try
-                    {
+            WriteEventLog(EventLogEntryType.Warning, "RadiusExtensionProcess2 called with params:", logMessage);
+            /* 
+             * Authentication request 
+             *      -ExtensionPoint: Authentication
+             *      -RequestType: AccessRequest
+             *      -ResponseType: Unknown
+             */
+            if ((control.ExtensionPoint == RadiusExtensionPoint.Authentication) && (control.RequestType == RadiusCode.AccessRequest)) {
+                if (control.ResponseType == RadiusCode.Unknown) {
+                    try {
                         /* Check if this request will be proxied by NPS */
-                        if (AttributeLookup(control.Request, RadiusAttributeType.Provider) == "2")
-                        {
+                        if (AttributeLookup(control.Request, RadiusAttributeType.Provider) == "2") {
                             /* Get NAS Client actual src IP and set NASIdentifier attrib with name defined on local config */
                             var clientIpVal = AttributeLookup(control.Request, RadiusAttributeType.SrcIPAddress);
-                            if (!string.IsNullOrEmpty(clientIpVal) && GetRADIUSClientsXMLconf().TryGetValue(clientIpVal, out string clientLocalName))
-                            {
+                            if (!string.IsNullOrEmpty(clientIpVal) && GetRADIUSClientsXMLconf().TryGetValue(clientIpVal, out string clientLocalName)) {
                                 var nasIdent = control.Request.FirstOrDefault(x => x.AttributeId.Equals((int)RadiusAttributeType.NASIdentifier));
-                                if (nasIdent != null)
-                                {
+                                if (nasIdent != null) {
                                     ((RadiusAttributeList)control.Request).Remove(nasIdent);
                                 }
                                 ((RadiusAttributeList)control.Request).Add(new RadiusAttribute(RadiusAttributeType.NASIdentifier, clientLocalName));
                             }
                         }
                     }
-                    catch (Exception ex)
-                    {
+                    catch (Exception ex) {
                         logMessage.Add($"Exception: {ex}");
-                        WriteEventLog(APP_NAME, "Trace", logMessage, EventLogEntryType.Warning);
+                        WriteEventLog(EventLogEntryType.Error, "Authentication error:", logMessage);
                     }
+                }
+                else {
+                    WriteEventLog(EventLogEntryType.Error, $"Control.ResponseType is '{control.RequestType}'");
                 }
             }
             /* Authorization request */
-            else if ((control.ExtensionPoint == RadiusExtensionPoint.Authorization) && (control.RequestType == RadiusCode.AccessRequest)) 
-            {
-                if (control.ResponseType == RadiusCode.AccessAccept)
-                {
+            else if ((control.ExtensionPoint == RadiusExtensionPoint.Authorization) && (control.RequestType == RadiusCode.AccessRequest)) {
+                if (control.ResponseType == RadiusCode.AccessAccept) {
                     /*
                      * Call MFA on already authorized request
                      * If MFA returns OK keep original disposition -> AccessAccept disposition
                      * If MFA returns KO override original disposition -> AccessReject 
                      */
-                    if (AttributeLookup(control.Response[RadiusCode.AccessAccept], RadiusAttributeType.CallbackId) == MFA_CALLBACK)
-                    {
+                    if (AttributeLookup(control.Response[RadiusCode.AccessAccept], RadiusAttributeType.CallbackId) == MFA_CALLBACK) {
                         userName = AttributeLookup(control.Request, RadiusAttributeType.UserName);
-                        if (!string.IsNullOrEmpty(userName))
-                        {
+                        if (!string.IsNullOrEmpty(userName)) {
                             logMessage.Add("-UserName: " + userName);
                             logMessage.Add("-NAS IPAddress: " + AttributeLookup(control.Request, RadiusAttributeType.NASIPAddress));
                             logMessage.Add("-Src IPAddress: " + AttributeLookup(control.Request, RadiusAttributeType.SrcIPAddress));
@@ -139,19 +131,17 @@ namespace NpsWrapperNET
                             /* Send mfa request, without domain */
                             //bool result = WG.AuthenticateUser(userName, null, true, customText).Result;
                             bool result = true;
-                            if (!result)
-                            {
+                            if (!result) {
                                 logMessage.Add("-MFA Process fail, setting AccessReject to NPS");
                                 logMessage.Add("+NPS request end");
                                 /* Set final disposition to AccessReject - Note that could be changed by other extensions */
                                 control.ResponseType = RadiusCode.AccessReject;
                             }
-                            else
-                            {
+                            else {
                                 logMessage.Add("-MFA Process success");
                                 logMessage.Add("+NPS request end");
                             }
-                            WriteEventLog(APP_NAME, "Trace", logMessage, EventLogEntryType.Information);
+                            WriteEventLog(EventLogEntryType.Information, "Trace", logMessage);
                         }
                     }
                 }
@@ -159,19 +149,16 @@ namespace NpsWrapperNET
             return 0;
         }
 
-        private static string AttributeLookup(IList<RadiusAttribute> attributesList, RadiusAttributeType attributeType )
-        {
+        private static string AttributeLookup(IList<RadiusAttribute> attributesList, RadiusAttributeType attributeType) {
             var a = attributesList.FirstOrDefault(x => x.AttributeId.Equals((int)attributeType));
-            if(a == null)
+            if (a == null)
                 return string.Empty;
             return (a.Value is byte[] val) ? Encoding.Default.GetString(val) : a.Value.ToString();
         }
         /* Get all attributes*/
-        private static List<string> AttributesToList(IList<RadiusAttribute> attributesList)
-        {
+        private static List<string> AttributesToList(IList<RadiusAttribute> attributesList) {
             var r = new List<string>();
-            foreach (var attrib in attributesList)
-            {
+            foreach (var attrib in attributesList) {
                 string attribName = (Enum.IsDefined(typeof(RadiusAttributeType), attrib.AttributeId)) ?
                     ((RadiusAttributeType)attrib.AttributeId).ToString()
                     : attrib.AttributeId.ToString();
@@ -187,29 +174,15 @@ namespace NpsWrapperNET
         /// <param name="subj">Event first row</param>
         /// <param name="subj_body">Event additional rows to append</param>
         /// <param name="level">Event Level</param>
-        private static void WriteEventLog(string src, string subj, List<string> subj_body, EventLogEntryType level)
-        {
-            const string log = "Application";
-            using (EventLog eventLog = new EventLog(log))
-            {
-                try
-                {
-                    if (!EventLog.SourceExists(src))
-                    {
-                        EventLog.CreateEventSource(src, log);
-                    }
-                    eventLog.Source = src;
-                    EventInstance eventInstance = new EventInstance(0, 0, level);
-                    subj_body.Insert(0, subj);
-                    EventLog.WriteEvent(src, eventInstance, subj_body.ToArray());
-                }
-                catch (Exception e)
-                {
-                    /* Could happen if program doesn't have rights to read/write registry
-                     *  1   from PS elevated prompt run [System.Diagnostics.EventLog]::CreateEventSource("APP_NAME", "Application")
-                     *  2   if needed regedit to "Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\{source} and set permission
-                     */
-                }
+        private static void WriteEventLog(EventLogEntryType level, string subj, List<string> subj_body = null) {
+            if (subj_body == null) {
+                subj_body = new List<string>();
+            }
+            using (EventLog eventLog = new EventLog("Application")) {
+                eventLog.Source = APP_NAME;
+                EventInstance eventInstance = new EventInstance(0, 0, level);
+                var body = string.Join(Environment.NewLine, subj_body);
+                EventLog.WriteEvent(eventLog.Source, eventInstance, new List<string>() { subj + Environment.NewLine + body }.ToArray());
             }
         }
 
@@ -217,8 +190,7 @@ namespace NpsWrapperNET
         /// Get clients defined in NPS by Server Data Object interface.
         /// Unfortunately call to GetServiceSDO(IASDATASTORE.DATA_STORE_LOCAL, "IAS") returns ACCESS DENIED so it's useless without finding further documentation
         /// </summary>
-        private static Dictionary<string,string> GetRADIUSClientsSDO()
-        {
+        private static Dictionary<string, string> GetRADIUSClientsSDO() {
             /* Attach to the local computer */
             var localServer = new SdoMachine();
             localServer.Attach(null);
@@ -231,8 +203,7 @@ namespace NpsWrapperNET
             /* Retrieve the clients collection */
             var ClientsCollection = sdoRadius.GetProperty(RADIUSPROPERTIES.PROPERTY_RADIUS_CLIENTS_COLLECTION);
             var result = new Dictionary<string, string>();
-            foreach (var client in ClientsCollection)
-            {
+            foreach (var client in ClientsCollection) {
                 var name = client.GetProperty(IASCOMMONPROPERTIES.PROPERTY_SDO_NAME);
                 var enabled = client.GetProperty(CLIENTPROPERTIES.PROPERTY_CLIENT_ENABLED);
                 var ip = client.GetProperty(CLIENTPROPERTIES.PROPERTY_CLIENT_ADDRESS);
@@ -243,8 +214,7 @@ namespace NpsWrapperNET
         /// <summary>
         /// Get clients defined in NPS by reading xml config file.
         /// </summary>
-        private static Dictionary<string, string> GetRADIUSClientsXMLconf()
-        {
+        private static Dictionary<string, string> GetRADIUSClientsXMLconf() {
             return RADIUSClientsXMLconf.Clients();
         }
 
@@ -252,8 +222,7 @@ namespace NpsWrapperNET
         /// Helper class for XML config parsing.
         /// Returns clients defined in XML or a local cache based on "cacheTime" defined value.
         /// </summary>
-        internal static class RADIUSClientsXMLconf
-        {
+        internal static class RADIUSClientsXMLconf {
             private const int cacheTime = 1;
             private const string xmlRadClientsNode = "/Root/Children/Microsoft_Internet_Authentication_Service/Children/Protocols/Children/Microsoft_Radius_Protocol/Children/Clients/Children/*";
             private readonly static string configFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "System32", "ias", "ias.xml");
@@ -261,15 +230,12 @@ namespace NpsWrapperNET
             private static DateTime lastCheckTime;
             private static Dictionary<string, string> clients = new Dictionary<string, string>();
 
-            public static Dictionary<string, string> Clients()
-            {
+            public static Dictionary<string, string> Clients() {
                 var now = DateTime.UtcNow;
-                if (now > lastCheckTime.AddMinutes(cacheTime))
-                {
+                if (now > lastCheckTime.AddMinutes(cacheTime)) {
                     lastCheckTime = now;
                     var lastmod = File.GetLastWriteTimeUtc(configFile);
-                    if (lastmod > configModTime)
-                    {
+                    if (lastmod > configModTime) {
                         configModTime = lastmod;
                         clients = ParseXmlClients();
                     }
@@ -277,23 +243,19 @@ namespace NpsWrapperNET
                 return clients;
             }
 
-            private static Dictionary<string, string> ParseXmlClients()
-            {
+            private static Dictionary<string, string> ParseXmlClients() {
                 var c = new Dictionary<string, string>();
-                using (var fileStream = new FileStream(configFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-                {
+                using (var fileStream = new FileStream(configFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
                     XPathDocument document = new XPathDocument(fileStream);
                     XPathNavigator navigator = document.CreateNavigator();
                     XPathNodeIterator nodeIterator = navigator.Select(xmlRadClientsNode);
 
-                    while (nodeIterator.MoveNext())
-                    {
+                    while (nodeIterator.MoveNext()) {
                         XPathNavigator node = nodeIterator.Current;
                         string name = node.GetAttribute("name", string.Empty);
                         string ip = node.SelectSingleNode("Properties/IP_Address")?.Value;
                         string enabled = node.SelectSingleNode("Properties/Radius_Client_Enabled")?.Value;
-                        if (enabled == "1")
-                        {
+                        if (enabled == "1") {
                             c.Add(ip, name);
                         }
                     }
